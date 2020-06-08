6d8c286b3e2cd45a18253d8e9097daee9fbdb726
Ankur <vasavada.ankur@gmail.com>
2020-06-08 08:51:01 -0400

    Connected to database
    Set up users route

--- Drinks-App/backend/routes/users.js
+++ Drinks-App/backend/routes/users.js
@@ -1,10 +1,10 @@
-const recipes = require("express").Router();
+const users = require("express").Router();
 const pool = require("../connection");
 
-recipes.get("/", (req, res) => {
+users.get("/", (req, res) => {
   pool.query("SELECT * FROM users").then((result) => {
     res.json(result.rows);
   });
 });
 
-module.exports = recipes;
+module.exports = users;
--- Drinks-App/backend/server.js
+++ Drinks-App/backend/server.js
@@ -3,10 +3,11 @@ const app = express();
 const port = 3000;
 
 const recipes = require("./routes/recipes");
-
-app.use("/recipes", recipes);
+const users = require("./routes/users");
 
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
+app.use("/users", users);
+app.use("/recipes", recipes);
 
 app.listen(port, () => console.log(`Listening on port: ${port}`));
