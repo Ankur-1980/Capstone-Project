aea9cbc9971666805f16bf81a218fa6f7c22490d
Ankur <vasavada.ankur@gmail.com>
2020-06-09 12:51:33 -0400

    Dropdown boxes
    Categories, Glassware, letters, and alcoholic or not

--- Drinks-App/src/app/components/recipe-book/recipe-search/recipe-search.component.ts
+++ Drinks-App/src/app/components/recipe-book/recipe-search/recipe-search.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeApiService } from '../../../services/recipeAPI.service';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ALPHABET } from './alphabet';
 
 @Component({
   selector: 'recipe-search',
@@ -11,17 +12,37 @@ export class RecipeSearchComponent implements OnInit {
   glassware: any;
   alcoholic: any;
   categories: any;
+  alphabet: string[] = ALPHABET;
+  filterForm: FormGroup;
+
   constructor(private fb: FormBuilder, private recipeAPI: RecipeApiService) {}
 
   ngOnInit(): void {
     this.recipeAPI.getGlassware().subscribe((data) => {
       this.glassware = data['drinks'];
+      console.log(this.glassware);
     });
     this.recipeAPI.getAlcoholic().subscribe((data) => {
       this.alcoholic = data['drinks'];
+      console.log(this.alcoholic);
     });
     this.recipeAPI.getCategories().subscribe((data) => {
       this.categories = data['drinks'];
+      console.log(this.categories);
     });
+
+    this.filterForm = this.fb.group({
+      glassware: [''],
+      categories: [''],
+      booze: [''],
+      alphabet: [''],
+    });
+    this.filterForm.valueChanges.subscribe((value) => console.log(value));
+  }
+
+  onSubmit() {}
+
+  resetForm() {
+    this.filterForm.reset();
   }
 }
