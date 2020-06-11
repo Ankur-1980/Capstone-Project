0e73f7f307ef9a163c45d5f0644fd79a56acdc45
Ankur <vasavada.ankur@gmail.com>
2020-06-11 12:36:45 -0400

    Preferences Form set up

--- Drinks-App/src/app/components/preferences/preferences-create/preferences-create.component.ts
+++ Drinks-App/src/app/components/preferences/preferences-create/preferences-create.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { PreferencesService } from '../preferences.service';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { CATEGORIES } from '../categories';
 
 @Component({
-  selector: 'app-preferences-create',
+  selector: 'preferences-create',
   templateUrl: './preferences-create.component.html',
-  styleUrls: ['./preferences-create.component.css']
+  styleUrls: ['./preferences-create.component.css'],
 })
 export class PreferencesCreateComponent implements OnInit {
+  categories = CATEGORIES;
+  itemForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private preferService: PreferencesService,
+    private fb: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.itemForm = this.fb.group({
+      info: [''],
+      category: [''],
+    });
   }
 
+  onSubmit() {
+    console.log(this.itemForm.value);
+
+    // if (form.invalid) {
+    //   return;
+    // }
+    // this.preferService.addItems(form.value);
+    // form.resetForm();
+  }
 }
