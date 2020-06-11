4bc652db65007519148fe5977b4f67a8b975ac50
Ankur <vasavada.ankur@gmail.com>
2020-06-11 17:30:46 -0400

    Drink Post Form set up
    working on backend

---
+++ Drinks-App/backend/routes/drink-posts.js
@@ -0,0 +1,4 @@
+const drinkPosts = require("express").Router();
+const database = require("../connection");
+
+module.exports = drinkPosts;
--- Drinks-App/backend/server.js
+++ Drinks-App/backend/server.js
@@ -5,6 +5,7 @@ const PORT = process.env.PORT || 3000;
 const recipes = require("./routes/recipes");
 const users = require("./routes/users");
 const preferences = require("./routes/preferences");
+const drinkPosts = require("./routes/drink-posts");
 
 // things coming from front end will be secure
 app.use(express.urlencoded({ extended: false }));
@@ -17,6 +18,7 @@ app.use(express.static(__dirname + "/public"));
 app.use("/api/users", users);
 app.use("/api/recipes", recipes);
 app.use("/api/preferences", preferences);
+app.use("/api/drink-posts", drinkPosts);
 
 // starting the server
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
