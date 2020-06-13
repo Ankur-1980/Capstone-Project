c9f6187f49a080078ec7c158ad915efcac69fe33
Ankur <vasavada.ankur@gmail.com>
2020-06-13 11:07:26 -0400

    working on sign up

--- Drinks-App/backend/server.js
+++ Drinks-App/backend/server.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
 const recipes = require("./routes/recipes");
 const users = require("./routes/users");
 const preferences = require("./routes/preferences");
 const drinkPosts = require("./routes/drink-posts");
+// do not need /.index because it looks for it by defaullt
+const auth = require("./auth");
 
 // things coming from front end will be secure
 app.use(express.urlencoded({ extended: false }));
@@ -19,6 +22,16 @@ app.use("/api/users", users);
 app.use("/api/recipes", recipes);
 app.use("/api/preferences", preferences);
 app.use("/api/drink-posts", drinkPosts);
+app.use("/api/auth", auth);
+
+// error handler
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: req.app.get("env") === "development" ? err : {},
+  });
+});
 
 // starting the server
 app.listen(PORT, () => console.log(`Listening on port: ${PORT}`));
