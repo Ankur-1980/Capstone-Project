ae83d81a79864fcf6b0adb400a23c606a5d35def
Ankur <vasavada.ankur@gmail.com>
2020-06-13 13:40:14 -0400

    3

--- Drinks-App/sql/users.sql
+++
@@ -1,8 +0,0 @@
--- insert into users
--- INSERT INTO users (user_id, first_name, last_name, username, email, password, bio, birthday)
--- VALUES (uuid_generate_v4(), 'John', 'Doe', 'johnnyboy', 'john@gmail.com', 'password', 'some stuff about john', '1989-11-12');
-
-
-INSERT INTO users (user_id, first_name, last_name, username, email, password, bio, birthday)
-VALUES (uuid_generate_v4(), 'John', 'Doe', 'johnnyboy', 'john@gmail.com', 'password', null, null);
-SELECT * FROM users
