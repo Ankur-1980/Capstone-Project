b658af6e877be979b8bc0996e67c263a1c60ec35
Ankur <vasavada.ankur@gmail.com>
2020-06-14 12:56:53 -0400

    Installed dotenv & bcrypt
    set up dotenv file

--- Drinks-App/backend/connection.js
+++ Drinks-App/backend/connection.js
@@ -1,10 +1,22 @@
+require("dotenv").config();
+
 const { Pool } = require("pg");
+
+// check to see if app is in production
+const isProduction = process.env.NODE_ENV === "production";
+
+const connectionString = `postgressql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:$${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
+
+// const credentials = new Pool({
+//   user: "postgres",
+//   password: "password",
+//   host: "localhost",
+//   port: 5432,
+//   database: "Capstone",
+//   ssl: false,
+// });
+
 const credentials = new Pool({
-  user: "postgres",
-  password: "password",
-  host: "localhost",
-  port: 5432,
-  database: "Capstone",
-  ssl: false,
+  connectionString: isProduction ? process.env.URL : connectionString,
 });
-module.exports = credentials;
+module.exports = { credentials };
--- Drinks-App/backend/routes/users.js
+++ Drinks-App/backend/routes/users.js
@@ -1,6 +1,7 @@
 const users = require("express").Router();
 const database = require("../connection");
 const bcrypt = require("bcrypt");
+const { response } = require("express");
 
 // get all users
 users.get("/", (req, res) => {
@@ -42,9 +43,22 @@ users.post("/register", async (req, res) => {
     )
     .then(() => {
       database.query("SELECT * FROM users").then((response) => {
-        res.status(201).json({ message: "User Added", users: response.rows });
+        res.status(200).json({ message: "User Added", users: response.rows });
       });
     });
+});
+
+// login in
+users.post("/login", async (req, res) => {
+  const loginName = req.body.userName;
+  const dbName = await database
+    .query(`SELECT username FROM users where username='${loginName}'`)
+    .then((response) => {
+      res.json(response.rows);
+    });
+  // console.log(loginName);
+
+  console.log(dbName);
 });
 
 module.exports = users;
