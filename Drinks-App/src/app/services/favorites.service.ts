e2215279e66ff28230d97d6cdcf57d916b180ffd
Ankur <vasavada.ankur@gmail.com>
2020-06-16 07:44:49 -0400

    set up favorites service

--- Drinks-App/src/app/services/favorites.service.ts
+++ Drinks-App/src/app/services/favorites.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FavoritesService {
+  favorites = [];
 
-  constructor() { }
+  constructor() {}
+
+  addToFavorites(drink) {
+    this.favorites.push(drink);
+    return this.favorites;
+  }
+
+  removeFromFavorites(drink) {
+    let index = this.favorites.findIndex(
+      (fav) => fav.idDrink === drink.idDrink
+    );
+    this.favorites.splice(index, 1);
+    return this.favorites;
+  }
+
+  getFavorites() {
+    return this.favorites;
+  }
+
+  containsFavorite(drink) {
+    return this.favorites.includes(drink);
+  }
 }
