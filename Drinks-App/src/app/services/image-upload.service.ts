52b9847d27bd79ec17a63845255815d1cb87a0e8
Ankur <vasavada.ankur@gmail.com>
2020-06-20 12:38:57 -0400

    Switching to back end

--- Drinks-App/src/app/services/image-upload.service.ts
+++ Drinks-App/src/app/services/image-upload.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ImageUploadService {
+  constructor() {}
 
-  constructor() { }
+  upLoadImage(image: File) {
+    const formData = new FormData();
+    formData.append('image', image);
+  }
 }
