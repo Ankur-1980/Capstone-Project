b233b3998a85e121f56bdd439919658afcf2344e
Ankur <vasavada.ankur@gmail.com>
2020-06-23 15:50:53 -0400

    Little fixes

--- Drinks-App/src/app/components/preferences/preferences-drinks/preferences-drinks.component.ts
+++ Drinks-App/src/app/components/preferences/preferences-drinks/preferences-drinks.component.ts
@@ -1,26 +1,25 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FavoritesService } from 'src/app/services/favorites.service';
 import { Subscription } from 'rxjs';
-import { MOCK_PREFER } from 'src/app/MOCK_DATA/mock-preferences';
 
 @Component({
   selector: 'preferences-drinks',
   templateUrl: './preferences-drinks.component.html',
-  styleUrls: ['./preferences-drinks.component.css'],
+  styleUrls: ['./preferences-drinks.component.css', '../preferences-pages.css'],
 })
 export class PreferencesDrinksComponent implements OnInit, OnDestroy {
-  favorites = MOCK_PREFER;
+  favorites = [];
   favSub: Subscription;
   constructor(private favoriteService: FavoritesService) {}
 
   ngOnInit(): void {
-    // this.favoriteService.getFavorites();
-    // this.favSub = this.favoriteService
-    //   .getFavoriteUpdateListener()
-    //   .subscribe((data) => {
-    //     this.favorites = data;
-    //     console.log(this.favorites);
-    //   });
+    this.favoriteService.getFavorites();
+    this.favSub = this.favoriteService
+      .getFavoriteUpdateListener()
+      .subscribe((data) => {
+        this.favorites = data;
+        console.log(this.favorites);
+      });
   }
 
   onDelete(drinkId) {
